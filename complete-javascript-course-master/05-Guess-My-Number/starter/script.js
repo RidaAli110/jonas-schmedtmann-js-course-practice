b77f29601be7339376b2e0b29d0e4c31f4cb4fe0
RidaAli110 <rida.webdev110@gmail.com>
2026-05-22 17:18:23 +0100

    Finished guess the number game

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -12,6 +12,7 @@ document.querySelector('.guess').value = 23;
  */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 
 
@@ -29,6 +30,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b367';
     document.querySelector('.number').style.width = '30rem';
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   } 
   // when guess is too high
   else if (guess > secretNumber) {
