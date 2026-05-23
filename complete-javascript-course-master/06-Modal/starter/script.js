9e1cebcdae472cd433c4e2af51b99ac3314f75de
RidaAli110 <rida.webdev110@gmail.com>
2026-05-23 18:08:01 +0100

    Completed Modal project

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,12 +1,30 @@
 'use strict';
+// getting the page elements we need
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
+
+//open the modal function 
+const openModal = function() {
+   console.log('Button Clicked');
     modal.classList.remove('hidden');
-  });
+    overlay.classList.remove('hidden');
 }
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// closing the modal function
+const closeModal = function() {
+   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+// esc key to exit modal
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+   closeModal();
+  }
+});
